86887e333886272ed02a5182a0b493f812f251b9
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2026-03-22 03:56:18 +0600

    fix: icon provider SVG rendering

--- src/providers/iconprovider.h
+++ src/providers/iconprovider.h
@@ -2,21 +2,37 @@
 
 #include <QQuickImageProvider>
 #include <QIcon>
+#include <QPainter>
 
 class IconProvider : public QQuickImageProvider
 {
 public:
-    IconProvider() : QQuickImageProvider(QQuickImageProvider::Pixmap) {}
+    IconProvider() : QQuickImageProvider(QQuickImageProvider::Image) {}
 
-    QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override
+    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override
     {
-        int sz = requestedSize.width() > 0 ? requestedSize.width() : 32;
+        int sz = (requestedSize.width() > 0) ? requestedSize.width() : 48;
+        QSize iconSize(sz, sz);
+
         QIcon icon = QIcon::fromTheme(id);
-        if (icon.isNull())
+        if (icon.isNull()) {
+            // Fallback chain
             icon = QIcon::fromTheme("text-x-generic");
-        QPixmap pix = icon.pixmap(sz, sz);
+            if (icon.isNull())
+                icon = QIcon::fromTheme("application-x-generic");
+        }
+
+        QImage img(iconSize, QImage::Format_ARGB32_Premultiplied);
+        img.fill(Qt::transparent);
+
+        if (!icon.isNull()) {
+            QPainter painter(&img);
+            icon.paint(&painter, 0, 0, sz, sz);
+            painter.end();
+        }
+
         if (size)
-            *size = pix.size();
-        return pix;
+            *size = iconSize;
+        return img;
     }
 };
